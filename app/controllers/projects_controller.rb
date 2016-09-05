dd1d0d7c244c0380f4467b4857c6f2086ebec0ab
Robin Hamill <hi@robinhamill.ca>
2016-09-05 12:25:35 -0400

    Comment for no sign_up option in Devise

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+before_action :authenticate_user!
+
 	def show
 		
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+# Use this to remove the user sign_up action
+	# devise_for :users, path_names: {
+	#   sign_up: ''
+	# }
+
   resources :projects
 
   root to: 'pages#home'
