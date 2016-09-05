b0435934582b134105c3fa0f5e61d14d6ac2eebe
Robin Hamill <hi@robinhamill.ca>
2016-09-05 11:44:46 -0400

    Add Projects Model, Controller and Views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,27 @@
+class ProjectsController < ApplicationController
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :projects
+
   root to: 'pages#home'
 
 end
