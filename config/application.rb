a959b219dcd5ae3d506ec27de5cb5646a2d550ea
Robin Hamill <hi@robinhamill.ca>
2016-09-05 12:45:03 -0400

    Setup image uploads

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,15 @@ module RobinHamill
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # config/application.rb
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+        access_key_id: ENV['AWS_KEY'],
+        secret_access_key: ENV['AWS_SECRET'],
+        bucket: "rh_portfoio-images"
+      }
+    }
   end
 end
