90c6cfc2d558c326db0cb43130be94959739e7a3
Robin Hamill <hi@robinhamill.ca>
2016-09-05 21:38:47 -0400

    Code cleanup

--- app/assets/javascripts/plugins.js
+++ app/assets/javascripts/plugins.js
@@ -295,7 +295,7 @@
     defaults = {
       scrollProperty: 'scroll',
       positionProperty: 'position',
-      horizontalScrolling: true,
+      horizontalScrolling: false,
       verticalScrolling: true,
       horizontalOffset: 0,
       verticalOffset: 0,
