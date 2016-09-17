92443745da02a88590d39b945ae83b3389a61eba
Robin Hamill <hi@robinhamill.ca>
2016-09-17 14:41:46 -0400

    Remove ability to register in prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
+  devise_for :users
 
 # Use this to remove the user sign_up action
-	devise_for :users, path_names: {
-	  sign_up: ''
-	}
+	# devise_for :users, path_names: {
+	#   sign_up: ''
+	# }
 
   resources :projects
 
