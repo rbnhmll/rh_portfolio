d757119b426fe833488d471fb632470a4986d9fe
Robin Hamill <hamillrobin@gmail.com>
2017-04-10 09:40:18 -0400

    Change imagemagik sizes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-	has_attached_file :screenshot, styles: { large: "900x600#", medium: "450x300#", thumb: "150x175#" }
+	has_attached_file :screenshot, styles: { large: "950x600#", medium: "500x350#", small: "300x200#", thumb: "175x150#" }
 
 	validates_attachment_content_type :screenshot, :content_type => /\Aimage\/.*\Z/
 
