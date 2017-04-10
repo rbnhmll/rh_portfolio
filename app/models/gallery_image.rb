8b46b6ad0898d20c9c74c577d6c5c9cf76cf18d0
Robin Hamill <hamillrobin@gmail.com>
2017-04-10 14:32:16 -0400

    Change banner image size

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -1,6 +1,6 @@
 class GalleryImage < ActiveRecord::Base
 
-	has_attached_file :image, styles: { large: "950x600>", medium: "450x300>"}, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { banner: "1800x450>", large: "950x600>", medium: "450x300>"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   belongs_to :project
